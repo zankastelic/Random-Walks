d06a6e9e4b581519b828db6b83c73629d83956f8
zankastelic <zkastelic15@gmail.com>
2022-03-06 22:20:26 +0100

    popravu

--- random_walks.R
+++ random_walks.R
@@ -18,15 +18,29 @@ nesim_sprehod <- function(p, spodnja_meja, zgornja_meja) {
   }
   vektor_vrednosti}
 
+######### za barve #######################
+colorPicker <- function(vsota, max_length,
+                        ls_color = c(178, 34, 34), ll_color = c(255, 204, 0),
+                        us_color = c(0, 0, 102), ul_color = c(102, 204, 225)) {
+  l <- length(values)
+  if (values[l] < 0) {
+    rgb_values <- (ls_color + (ll_color - ls_color) * l / max_length) / 255
+  } else {
+    rgb_values <- (us_color + (ul_color - us_color) * l / max_length) / 255
+  }
+  rgb(rgb_values[1], rgb_values[2], rgb_values[3])
+}
+
+
 vektor_vrednosti <- c(0)
 vsota <- 0
 p = 0.50
 spodnja_meja <- -10
 zgornja_meja <- 10
-a <- sprehod(p,spodnja_meja,zgornja_meja)
-N <- 10
+a <- sim_sprehod(p,spodnja_meja,zgornja_meja)
+N <- 100
 seznam <- replicate(N, sim_sprehod(p, spodnja_meja, zgornja_meja))
-
+set.seed(77)
 max_length <- max(sapply(seznam, length))
 # make plot
 plot(c(1, max_length), c(spodnja_meja, zgornja_meja), type = "n",
@@ -37,4 +51,4 @@ for (i in 1:N) {
 abline(h = 0, lty = "dashed")
 abline(h = spodnja_meja, lwd = 2)
 abline(h = zgornja_meja, lwd = 2)
-```
+
